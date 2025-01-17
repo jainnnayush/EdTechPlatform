103bb5142f576db2f14f96ad25e8d79504e6120b
Ayush Jain <ayushjain13102001@gmail.com>
2025-01-17 17:00:47 +0530

    password reset backend

--- SERVER/routes/User.js
+++ SERVER/routes/User.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {auth} =require("../middlewares/Auth.js");
-
+const {resetPasswordToken,resetPassword}=require("../controllers/ResetPassword.js")
 const {sendOTP,changePassword,signup,login} = require('../controllers/Auth.js');
+
+// ********************************************************************************************************
+//                                      Authentication routes
+// ********************************************************************************************************
+
 
 // route for sending otp to user's mail
 router.post('/sendOTP',sendOTP);
@@ -15,5 +20,15 @@ router.post('/signup',signup);
 
 // route for login
  router.post('/login',login);
+
+// ********************************************************************************************************
+//                                      Reset Password
+// ********************************************************************************************************
+
+// route for token generation to change the password
+router.post('/reset-password-token',resetPasswordToken);
+
+// route for changing the password
+router.post('/reset-password',resetPassword);
 
 module.exports = router;
